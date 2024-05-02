6fa51b1f5b450d185b68ab30a31ed18f8b5b94d0
SebdGv <153835426+SebdGv@users.noreply.github.com>
2024-05-02 12:16:08 +0200

    Add files via upload
    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import Pages from "vite-plugin-pages";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    Pages({
+      dirs: "src/pages",
+    }),
+  ],
+});
