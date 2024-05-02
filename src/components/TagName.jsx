4a94668feeb7fbab337e3dbc73d6aeae858a58e1
bes dgv <bes.dgv@gmail.com>
2024-05-02 12:21:51 +0200

    add src & public

---
+++ src/components/TagName.jsx
@@ -0,0 +1,3 @@
+export default function TagName() {
+  return <div>tagName</div>;
+}
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "~react-pages";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./index.css";
+console.log(routes);
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Header />
+    <RouterProvider router={router} />
+    <Footer />
+  </React.StrictMode>
+);
