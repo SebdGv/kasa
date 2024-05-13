89325046c79a45ae827249661dd71dad8fc10d36
bes dgv <bes.dgv@gmail.com>
2024-05-13 10:20:47 +0200

    add carousel condition if 1 img

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -11,39 +11,40 @@ export default function Carousel({ images }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   if (!images || images.length === 0) {
-    // Afficher un message ou un placeholder si aucune image n'est disponible
     // eslint-disable-next-line react/no-unescaped-entities
     return <div>Pas d'images disponibles</div>;
   }
 
   const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length); // Incrémente l'index et boucle à 0 si on dépasse le nombre d'images
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const prevImage = () => {
     setCurrentImageIndex((prevIndex) => {
-      if (prevIndex === 0) return images.length - 1; // Si l'index est 0, revient à la dernière image
-      return prevIndex - 1; // Décrémente l'index
+      if (prevIndex === 0) return images.length - 1;
+      return prevIndex - 1;
     });
   };
   return (
     <>
       <div className="carousel">
-        <div className="carouselNav">
-          <FontAwesomeIcon
-            onClick={prevImage}
-            icon={faChevronLeft}
-            className="navArrow left-arrow"
-          />
-          <p className="carousel-number">
-            {currentImageIndex + 1}/{images.length}
-          </p>
-          <FontAwesomeIcon
-            onClick={nextImage}
-            icon={faChevronRight}
-            className="navArrow right-arrow"
-          />
-        </div>
+        {images.length > 1 && (
+          <div className="carouselNav">
+            <FontAwesomeIcon
+              onClick={prevImage}
+              icon={faChevronLeft}
+              className="navArrow left-arrow"
+            />
+            <p className="carousel-number">
+              {currentImageIndex + 1}/{images.length}
+            </p>
+            <FontAwesomeIcon
+              onClick={nextImage}
+              icon={faChevronRight}
+              className="navArrow right-arrow"
+            />
+          </div>
+        )}
         <img
           className="carouselImg"
           src={images[currentImageIndex]}
