3f9695bdd1723f0a7b7046c39cca723864f78d97
bes dgv <bes.dgv@gmail.com>
2024-05-13 14:36:17 +0200

    add à propos content

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -10,19 +10,19 @@ export default function About() {
         <div className="container">
           <Dropdown
             title="Fiabilité"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur minima autem distinctio iusto adipisci quibusdam neque nam."
+            content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
           />
           <Dropdown
             title="Respect"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur minima autem distinctio iusto adipisci quibusdam neque nam."
+            content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement descriminatoire ou de pertubation du voisinage entrainera une exclusion de notre plateforme."
           />
           <Dropdown
             title="Services"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur minima autem distinctio iusto adipisci quibusdam neque nam."
+            content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement descriminatoire ou de pertubation du voisinage entrainera une exclusion de notre plateforme."
           />
           <Dropdown
             title="Sécurité"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur minima autem distinctio iusto adipisci quibusdam neque nam."
+            content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
           />
         </div>
       </div>
